4bfe1e83ff39e2937f209ea7a587f6e3aa469eee
Ajaybazilissac <66030502+Ajaybazilissac@users.noreply.github.com>
2020-09-22 10:57:29 +0530

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,90 @@
+
+$(function(){
+    var $registrationform=$("#rejistration");
+    if($registrationform.length){
+        $registrationform.validate({
+            rules:{
+                FullName:{
+                    required:true
+                },
+                Email:{
+                    required:true,
+                    email:true
+                },
+                Subject:{
+                    required:true
+                },
+                Message:{
+                    required:true
+                }
+            },
+            messages:{
+                FullName:{
+                    required:"Name is mandatory."
+                },
+                Email:{
+                    required:"Email is mandatory.",
+                    email:'Please enter valid email.'
+                },
+                Subject:{
+                    required:"Subject is mandatory."
+                },
+                Message:{
+                    required:"Please add message in short."
+                }
+            }
+        })
+    }
+})
+
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover({
+      html: true,
+      placement: "right",
+      trigger: "click",
+    });
+    $("#buttonprimary").click(function () {
+      var fullName = $("#FullName").val();
+      var email = $("#Email").val();
+      var phone = $("#Subject").val();
+      var message = $("#Message").val();
+      // console.log("FullName : " + fullName);
+      // console.log("Email : " + email);
+      // console.log("Mobile No : " + phone);
+      // console.log("Message : " + message);
+      if (validator()) {
+        
+        console.log("Form Submitted");
+        $("#Myrejistrationorm").submit();
+        alert("Thank you for Submission");
+        //var formData = $(this).serializeArray();
+        //console.log(formData);
+      } else {
+        alert("Error While submitting");
+      //   console.log("error while submitting");
+      }
+    });
+  
+    function validator() {
+      var fullName = $("#FullName").val();
+      var email = $("#Email").val();
+      var phone = $("#Subject").val();
+      var message = $("#Message").val();
+      var emailValidator = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      //console.log("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;");
+      //console.log("FullName : "+fullName);
+      // console.log("Email : "+email);
+    /*  if (!emailValidator.test(email)) {
+        alert("Invalid Email ID...!!!!!");
+        return false;
+      } else if (phone.length !== 10) {
+        alert("Please enter the correct mobile No...!!!!! ");
+      } else if (message === "") {
+        alert("Enter Something");
+        return false;
+      } else {
+        return true;
+      }*/
+    }
+    
+  });
